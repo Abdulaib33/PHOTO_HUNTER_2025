722d3ab06bf89bc042385412c51a6befad4e0f3a
Abdoulaye <Abdulaibayo@outlook.fr>
2025-04-17 15:04:48 +0200

    structure MVC de base

---
+++ app/config/params.php
@@ -0,0 +1,13 @@
+<?php 
+
+// Initialisation des zones dynamiques
+
+$content = "";
+
+
+
+// Parametres de connexion
+define("DB_HOST", 'http://localhost/');
+define("DB_NAME", 'photo_hunter');
+define("DB_USER", "root");
+define("DB_PASSWORD", "root");
